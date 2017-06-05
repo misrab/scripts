4d0fa8992cc5000a19c71aace9c163fe2017e08e
misrab <faizullah.misrab@gmail.com>
2017-06-05 11:14:06 +0800

    Create setup.sh

---
+++ tensorflow/setup.sh
@@ -0,0 +1,7 @@
+# install virtualenv if not already instgalled
+# sudo apt-get install python-pip python-dev python-virtualenv 
+
+
+# setup virtualenv for tensorflow
+virtualenv --system-site-packages ~/tensorflow
+
