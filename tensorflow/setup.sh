f981a0b54ddcd906af1022f27791d545dcf9fb0d
Misrab <faizullah.misrab@gmail.com>
2020-04-22 22:09:35 +0100

    remove tensorflow. add vim image

--- tensorflow/setup.sh
+++
@@ -1,7 +0,0 @@
-# install virtualenv if not already instgalled
-# sudo apt-get install python-pip python-dev python-virtualenv 
-
-
-# setup virtualenv for tensorflow
-virtualenv --system-site-packages ~/tensorflow
-
