e9b1cc93cf6bfd1cee1c3fb50cd84ab6f8680372
monkfish <faizullah.misrab@gmail.com>
2021-10-28 12:00:54 +0100

    add docker installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -35,9 +35,28 @@ sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 
 
+# install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update -y
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
 # install golang and create code repo
 # NOTE GOPATH and GOROOT in misrabrc
 mkdir ~/code && mkdir~/code/src && mkdir ~/code/src/github.com && mkdir ~/code/src/github.com/misrab
+
 
 # UPDATE - install manually (acutally)
 # install golang manually
