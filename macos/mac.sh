c4c5f87f392df9199185d84e993a137550342452
monkfish <faizullah.misrab@gmail.com>
2022-10-10 18:25:51 +0100

    add hugo

--- macos/mac.sh
+++ macos/mac.sh
@@ -55,3 +55,6 @@ mkdir ~/code && mkdir~/code/src && mkdir ~/code/src/github.com && mkdir ~/code/s
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
+
+# hugo blog
+brew install hugo
