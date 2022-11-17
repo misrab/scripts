e1f5c78b9ec96479e4f2e6fa8bbab58b65434606
monkfish <faizullah.misrab@gmail.com>
2022-11-17 15:37:11 +0000

    brew install golang

--- macos/mac.sh
+++ macos/mac.sh
@@ -49,6 +49,7 @@ sudo chpass -s $BASH_LOCATION $USER
 
 # install golang and create code repo
 # NOTE GOPATH and GOROOT in misrabrc
+brew install golang
 mkdir ~/code && mkdir~/code/src && mkdir ~/code/src/github.com && mkdir ~/code/src/github.com/misrab
 
 
